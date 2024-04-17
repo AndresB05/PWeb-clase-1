c709cadc69e0fa0a44af8d0285d77edbfa7c06a4
AndresB05 <127789561+AndresB05@users.noreply.github.com>
2024-04-16 21:14:32 -0500

    Add files via upload

---
+++ crudProgramacion/src/main/java/com/unac/crudProgramacion/services/UserService.java
@@ -0,0 +1,11 @@
+package com.unac.crudProgramacion.services;
+
+import com.unac.crudProgramacion.dto.UserDTO;
+
+import java.util.List;
+
+public interface UserService {
+
+    UserDTO saveUser(UserDTO userDTO);
+    List<UserDTO> getUser();
+}
